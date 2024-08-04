27d99cc4f5aa0369ab5445f8aa1f5a3ecb1acbf6
adit2998 <36098998+adit2998@users.noreply.github.com>
2024-08-04 03:32:29 -0400

    Playground branch

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,19 +2,19 @@ function ProfileCard(props){
     return (
         <div className="card">
             <div className="card-image">
-                <figure class="image is-1by1">
+                <figure className="image is-1by1">
                     <img
                         src={props.image}
                         alt="Placeholder image"
                     />
                 </figure>
             </div>
-            <div class="card-content">
-                <div class="media">
+            <div className="card-content">
+                <div className="media">
                 
-                    <div class="media-content">
-                        <p class="title is-4">Hero: {props.superhero}</p>
-                        <p class="subtitle is-6">Identity: {props.identity}</p>
+                    <div className="media-content">
+                        <p className="title is-4">Hero: {props.superhero}</p>
+                        <p className="subtitle is-6">Identity: {props.identity}</p>
                     </div>
                 </div>                
             </div>
